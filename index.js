2164d6c0cc58b4a8bc7f40d9b6b4499d5eb78159
ceboulba <antoine@archipicture.fr>
2020-03-10 21:37:18 +0100

    1 file modified

--- index.js
+++ index.js
@@ -66,24 +66,24 @@ canvas.classList.add('hide')
 
 const scene = createScene()
 
-const check = () => {
-  num <= 2 ?( () => {
-    canvas.classList.add('hide');
-    imgBox.classList.remove('hide');
-    viewType.classList.add('hide')
-  })()
-    :( () => {
-      canvas.classList.remove('hide')
-      viewType.classList.remove('hide')
-      imgBox.classList.add('hide')
-    })()
-  // scene.render()
-  num <= 2 ? imgView.src = imgs[num] : null
-  //num === 8 ? imgView.src = imgs[8] : null
-  //num === 9 ? imgView.src = imgs[9] : null
-}
+// const check = () => {
+//   num <= 2 ?( () => {
+//     canvas.classList.add('hide');
+//     imgBox.classList.remove('hide');
+//     viewType.classList.add('hide')
+//   })()
+//     :( () => {
+//       canvas.classList.remove('hide')
+//       viewType.classList.remove('hide')
+//       imgBox.classList.add('hide')
+//     })()
+//   // scene.render()
+//   num <= 2 ? imgView.src = imgs[num] : null
+//   //num === 8 ? imgView.src = imgs[8] : null
+//   //num === 9 ? imgView.src = imgs[9] : null
+// }
 
-//check()
+// //check()
 
 const next = () => {
   num < imgs.length - 1 ?
