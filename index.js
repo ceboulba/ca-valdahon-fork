8f87443ac9b57893dc91799810a9ebc7baab9471
ceboulba <antoine@archipicture.fr>
2020-04-15 22:28:54 +0200

    2 files modified

--- index.js
+++ index.js
@@ -1,73 +1,84 @@
-'use strict'
-import './style.css'
-import * as BABYLON from 'https://preview.babylonjs.com/babylon.js'
-const canvas = document.getElementById('renderCanvas')
-const imgAxo = document.querySelector('.img-axo')
+"use strict";
+import "./style.css";
+import * as BABYLON from "babylonjs";
+import * as GUI from "babylonjs-gui"
+const canvas = document.getElementById("renderCanvas");
+const imgAxo = document.querySelector(".img-axo");
 //const imgView = document.getElementById('img-view')
 //const viewType = document.querySelector('.view-type')
-const imageName = document.querySelector('.imageName')
+const imageName = document.querySelector(".imageName");
 //console.log(imageName.innerHTML)
 
 const imgs = [
-  'https://res.cloudinary.com/archipicture/image/upload/v1583261742/ca-valdahon/valdahon-axo-01.jpg',
-  'https://res.cloudinary.com/archipicture/image/upload/v1583261711/ca-valdahon/valdahon-axo-02.jpg',
-  'https://res.cloudinary.com/archipicture/image/upload/v1583261712/ca-valdahon/valdahon-axo-03.jpg',
-  'https://res.cloudinary.com/archipicture/image/upload/v1583256612/ca-valdahon/cafc-Valdhon-02_View01.jpg',
-  'https://res.cloudinary.com/archipicture/image/upload/v1583257239/ca-valdahon/cafc-Valdhon-02_View02.jpg',
-  'https://res.cloudinary.com/archipicture/image/upload/v1583257643/ca-valdahon/cafc-Valdhon-02_View04.jpg',
-  'https://res.cloudinary.com/archipicture/image/upload/v1583256617/ca-valdahon/cafc-Valdhon-02_View05.jpg',
-  'https://res.cloudinary.com/archipicture/image/upload/v1583257068/ca-valdahon/cafc-Valdhon-02_View06.jpg',
-  'https://res.cloudinary.com/archipicture/image/upload/v1583263143/ca-valdahon/cafc-Valdhon-02_View07.jpg',
-]
-const imgName= [
-  'Axo R.D.C 1',
-  'Axo R.D.C 2',
-  'Axo étage',
-  'Espace libre service bancaire',
-  'Espace attente / carré',
-  'Espace collaborateurs R.D.C',
-  'Attente étage',
-  'Circulation étage',
-  'Espace collaborateurs étage',
-]
+  "https://res.cloudinary.com/archipicture/image/upload/v1583261742/ca-valdahon/valdahon-axo-01.jpg",
+  "https://res.cloudinary.com/archipicture/image/upload/v1583261711/ca-valdahon/valdahon-axo-02.jpg",
+  "https://res.cloudinary.com/archipicture/image/upload/v1583261712/ca-valdahon/valdahon-axo-03.jpg",
+  "https://res.cloudinary.com/archipicture/image/upload/v1583256612/ca-valdahon/cafc-Valdhon-02_View01.jpg",
+  "https://res.cloudinary.com/archipicture/image/upload/v1583257239/ca-valdahon/cafc-Valdhon-02_View02.jpg",
+  "https://res.cloudinary.com/archipicture/image/upload/v1583257643/ca-valdahon/cafc-Valdhon-02_View04.jpg",
+  "https://res.cloudinary.com/archipicture/image/upload/v1583256617/ca-valdahon/cafc-Valdhon-02_View05.jpg",
+  "https://res.cloudinary.com/archipicture/image/upload/v1583257068/ca-valdahon/cafc-Valdhon-02_View06.jpg",
+  "https://res.cloudinary.com/archipicture/image/upload/v1583263143/ca-valdahon/cafc-Valdhon-02_View07.jpg"
+];
+const imgName = [
+  "Axo R.D.C 1",
+  "Axo R.D.C 2",
+  "Axo étage",
+  "Espace libre service bancaire",
+  "Espace attente / carré",
+  "Espace collaborateurs R.D.C",
+  "Attente étage",
+  "Circulation étage",
+  "Espace collaborateurs étage"
+];
 //GUI
-    //var advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
+var advancedTexture = GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
 
+let num = 0;
 
-let num = 0
+const btnPrev = document
+  .getElementById("btnPrev")
+  .addEventListener("click", () => prev());
+const btnNext = document
+  .getElementById("btnNext")
+  .addEventListener("click", () => next());
 
-const btnPrev = document.getElementById('btnPrev').addEventListener('click', () => prev())
-const btnNext = document.getElementById('btnNext').addEventListener('click', () => next())
+var engine = new BABYLON.Engine(canvas, true);
 
-var engine = new BABYLON.Engine(canvas, true)
+var createScene = function() {
+  var scene = new BABYLON.Scene(engine);
+  var camera = new BABYLON.ArcRotateCamera(
+    "Camera",
+    -Math.PI / 2,
+    Math.PI / 2,
+    5,
+    BABYLON.Vector3.Zero(),
+    scene
+  );
+  camera.attachControl(canvas, true);
+  camera.inputs.attached.mousewheel.detachControl(canvas);
 
-var createScene = function () {
-    var scene = new BABYLON.Scene(engine);
-    var camera = new BABYLON.ArcRotateCamera("Camera", -Math.PI / 2,  Math.PI / 2, 5, BABYLON.Vector3.Zero(), scene);
-    camera.attachControl(canvas, true);
-    camera.inputs.attached.mousewheel.detachControl(canvas);
+  var dome = new BABYLON.PhotoDome(
+    "testdome",
+    imgs[num],
+    {
+      resolution: 32,
+      size: 1000
+    },
+    scene
+  );
 
-    var dome = new BABYLON.PhotoDome(
-        "testdome",
-        imgs[num],
-        {
-            resolution: 32,
-            size: 1000
-        },
-        scene
-    );
+  //dome.imageMode = BABYLON.PhotoDome.MODE_SIDEBYSIDE;
+  //     if(num <= 2 ) {
+  // canvas.classList.add('hide')
+  // }else {
+  //   canvas.classList.remove('hide')
+  // }
 
-    //dome.imageMode = BABYLON.PhotoDome.MODE_SIDEBYSIDE;
-//     if(num <= 2 ) {
-// canvas.classList.add('hide')
-// }else {
-//   canvas.classList.remove('hide')
-// }
-
-    return scene;
+  return scene;
 };
 
-const scene = createScene()
+const scene = createScene();
 
 // const check = () => {
 //   num <= 2 ?( () => {
@@ -89,26 +100,22 @@ const scene = createScene()
 // //check()
 
 const next = () => {
-  num < imgs.length - 1 ?
-    num++
-    : num = 0
-  scene = createScene()
-}
+  num < imgs.length - 1 ? num++ : (num = 0);
+  scene = createScene();
+};
 
 const prev = () => {
-  num === 0 ?
-    num = imgs.length - 1
-    : num--
-  scene = createScene()
-}
+  num === 0 ? (num = imgs.length - 1) : num--;
+  scene = createScene();
+};
 
-engine.runRenderLoop(function () {
-    imageName.innerHTML = imgName[num]
-  scene.render()
-})
+engine.runRenderLoop(function() {
+  imageName.innerHTML = imgName[num];
+  scene.render();
+});
 
-if(num!==0){
-window.addEventListener('resize', function () {
-  engine.resize()  
-})
+if (num !== 0) {
+  window.addEventListener("resize", function() {
+    engine.resize();
+  });
 }
